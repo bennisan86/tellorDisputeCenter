d90ca54955f323170cda348749b2dc2c5b3f8b96
bennisan86 <info@bennisan.be>
2021-02-18 15:49:08 +0100

    Init commit migration page

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Mining from 'views/mining/Mining';
 import Voting from 'views/voting/Voting';
+import Migration from 'views/migration/Migration';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Mining} />
     <Route path="/voting" exact component={Voting} />
     <Route path="/mining" exact component={Mining} />
+    <Route path="/migration" exact component={Migration} />
     <Route path="*" component={Mining} />
   </Switch>
 );
--- src/components/shared/HeaderNav.js
+++ src/components/shared/HeaderNav.js
@@ -1,7 +1,7 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Layout } from 'antd';
 import { RightCircleOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { Link,useLocation } from 'react-router-dom';
 
 import { UserContext } from 'contexts/User';
 import { truncateAddr } from 'utils/helpers';
@@ -15,36 +15,57 @@ const HeaderNav = () => {
   const { Header } = Layout;
   const [currentUser] = useContext(UserContext);
   const [logo, setLogo] = useState(tellorLogoDark);
+  const location = useLocation();
 
   return (
     <Header>
       <div className="Header">
-        <div className="BrandLink">
-          <Link to="/">
-            <img alt="tellor-logo" src={logo} />
-            <span>dispute center</span>
-          </Link>
-        </div>
-        <div className="Header__Nav">
-          <ModeSwitcher setLogo={setLogo} />
-          {/* <Link to="/tellorUpgrade">Tellor V2.6!</Link> */}
-          <Link to="/voting">Voting</Link>
-          <Link to="/mining">Mining</Link>
-          <a
-            href="https://prices.tellorscan.com/"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Price Viewer <RightCircleOutlined />
-          </a>
-          {!currentUser ? null : (
-            <span>{truncateAddr(currentUser.address)}</span>
-          )}
-          <Web3SignIn />
-
-          <NetworkSwitcher />
-        </div>
+      {location.pathname === "/migration" ?
+      <>
+      <div className="BrandLink">
+        <Link to="/">
+          <img alt="tellor-logo" src={logo} />
+          <span>migration</span>
+        </Link>
       </div>
+      <div className="Header__Nav">
+        {!currentUser ? null : (
+          <span>{truncateAddr(currentUser.address)}</span>
+        )}
+        <Web3SignIn />
+        <NetworkSwitcher />
+      </div>
+      </>
+      :
+      <>
+      <div className="BrandLink">
+        <Link to="/">
+          <img alt="tellor-logo" src={logo} />
+          <span>dispute center</span>
+        </Link>
+      </div>
+      <div className="Header__Nav">
+        <ModeSwitcher setLogo={setLogo} />
+        {/* <Link to="/tellorUpgrade">Tellor V2.6!</Link> */}
+        <Link to="/voting">Voting</Link>
+        <Link to="/mining">Mining</Link>
+        <a
+          href="https://prices.tellorscan.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Price Viewer <RightCircleOutlined />
+        </a>
+        {!currentUser ? null : (
+          <span>{truncateAddr(currentUser.address)}</span>
+        )}
+        <Web3SignIn />
+        <NetworkSwitcher />
+      </div>
+      </>
+      }
+      </div>
+      
     </Header>
   );
 };
